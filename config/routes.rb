06ea4f4eea2a423bf2b443fb59c5220bfe12bcc1
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-25 07:43:44 -0400

    Used session to manage cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'  # Sets the root path to the index action of the ProductsController
+
+  post 'add', to: 'products#add'  # Creates a route to handle adding products to the cart
 end
